267c8c63a317a0ac348c54c509225f85048599dc
kalvish <kalanvishwanath@gmail.com>
2015-09-28 00:28:48 +0800

    inheritance

--- oop_swift_fun.playground/Contents.swift
+++ oop_swift_fun.playground/Contents.swift
@@ -4,12 +4,20 @@ import UIKit
 
 var str = "Hello, playground"
 
-var person = Person(name: "test")
+//
+//Sources->Inheritance
+//
 
+//Create a Person Class Instance
+var person = Person(name: "test")
 print(person.getName())
 
+//Create a Student Class Instance, which inherits 'Person' Class. Therefore I can initialize the 'Student' Class with 'Person' initializer unless 'Student' Class has a overiding initializer.
 var student = Student(name: "I'm a student")
 
+//Ofcourse I can use the methods inherit from 'Person' Class
 print(student.getName())
+
+//Q1. How can you overide initializer in 'Student' Class?
 
 
